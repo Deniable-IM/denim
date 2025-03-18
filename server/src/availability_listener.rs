79738b910b2de4bba74fa563d0a5f46b9064cf99
Andreas Knudsen Alstrp <aalstr20@student.aau.dk>
2025-03-18 13:30:24 +0100

    Added notify to AvailabilityListener

--- server/src/availability_listener.rs
+++ server/src/availability_listener.rs
@@ -1,7 +1,32 @@
 use axum::async_trait;
+use libsignal_core::ProtocolAddress;
+use std::{collections::HashMap, sync::Arc};
+use tokio::sync::Mutex;
+
+pub type ListenerMap<T> = Arc<Mutex<HashMap<String, Arc<Mutex<T>>>>>;
 
 #[async_trait]
 pub trait AvailabilityListener: Send + 'static {
     async fn send_cached(&mut self) -> bool;
     async fn send_persisted(&mut self) -> bool;
+}
+
+pub async fn notify_cached<T>(listeners: ListenerMap<T>, address: &ProtocolAddress)
+where
+    T: AvailabilityListener,
+{
+    let queue_name = format!("{}::{}", address.name(), address.device_id());
+    if let Some(listener) = listeners.lock().await.get(&queue_name) {
+        listener.lock().await.send_cached().await;
+    }
+}
+
+pub async fn notify_persisted<T>(listeners: ListenerMap<T>, address: &ProtocolAddress)
+where
+    T: AvailabilityListener,
+{
+    let queue_name = format!("{}::{}", address.name(), address.device_id());
+    if let Some(listener) = listeners.lock().await.get(&queue_name) {
+        listener.lock().await.send_persisted().await;
+    }
 }
