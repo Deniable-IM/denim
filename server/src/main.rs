f78ec528884e43b7169d01f6f21dc9ffba149cea
Andreas Knudsen Alstrup <aalstr20@student.aau.dk>
2025-06-01 17:23:43 +0200

    Set threads to max for CPU

--- server/src/main.rs
+++ server/src/main.rs
@@ -13,9 +13,20 @@ mod storage;
 mod test_utils;
 mod validators;
 
-#[tokio::main]
-pub async fn main() {
-    let use_tls = !env::args().any(|arg| arg == "--no-tls");
-    println!("Using tls: {}", use_tls);
-    signal_server::start_server(use_tls).await.unwrap();
+fn main() {
+    // Use a minimum of 4 threads
+    let cpus = num_cpus::get().max(4);
+    println!("Using {} threads", cpus);
+
+    let runtime = tokio::runtime::Builder::new_multi_thread()
+        .worker_threads(cpus)
+        .enable_all()
+        .build()
+        .unwrap();
+
+    runtime.block_on(async {
+        let use_tls = !env::args().any(|arg| arg == "--no-tls");
+        println!("using tls: {}", use_tls);
+        signal_server::start_server(use_tls).await.unwrap();
+    })
 }
